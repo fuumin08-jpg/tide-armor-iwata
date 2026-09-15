b187348b7d0f99030de7f3d395f60067a5442ed7
fuumin08-jpg <fuumin.08@gmail.com>
2026-09-15 20:11:55 +0900

    Create Iwata tackle inventory

---
+++ inventory.js
@@ -0,0 +1,23 @@
+// TIDE ARMOR - IWATA
+// Owned tackle inventory
+// Unknown specifications are intentionally left null.
+
+window.TIDE_ARMOR_INVENTORY = {
+  version: 1,
+  updatedAt: "2026-09-15",
+  tackles: [
+    {
+      id: "iwata_sonio93m_certate6000xh",
+      rodMaker: "ZENAQ",
+      rod: "MUTHOS Sonio 93M RG Guide",
+      reelMaker: "DAIWA",
+      reel: "CERTATE SW 6000-XH",
+      line: null,
+      leader: null,
+      knot: null,
+      solidRing: null,
+      swivel: null
+    }
+  ],
+  lures: []
+};
